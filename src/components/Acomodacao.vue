3f0e9bcbc7bf678b4f9166b733d2191f6c2fd313
jose edmar de siqueira <edmar.ade@gmail.com>
2023-02-25 14:40:36 -0300

    commit

--- src/components/Acomodacao.vue
+++ src/components/Acomodacao.vue
@@ -1,35 +1,31 @@
 <template>
-  <div class="flex-img">
-    <div
-      class="foto-hover"
-      v-for="acomodacao in acomodacoes"
-      :key="acomodacao.titulo"
-    >
-      <div class="foto-info" :data-id="acomodacao.id" @click="selecionarAcomodacao">
-        <h5>{{ acomodacao.titulo }}</h5>
-        <!-- <p>{{ acomodacao.descricao }}</p> -->
-      </div>
-      <img :src="`${acomodacao.src}`" alt="" />
+    <div class="flex-img">
+        <div class="foto-hover" v-for="acomodacao in acomodacoes" :key="acomodacao.titulo">
+            <div class="foto-info" :data-id="acomodacao.id" @click="selecionarAcomodacao">
+                <h5>{{ acomodacao.titulo }}</h5>
+                <!-- <p>{{ acomodacao.descricao }}</p> -->
+            </div>
+            <img :src="`${acomodacao.src}`" alt="" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import { acomodacoes } from "../helpers/AcomodacaoData";
-import { Acomodacao } from "@/class/Acomodacao";
+import { acomodacoes } from '../helpers/AcomodacaoData';
+import { Acomodacao } from '@/class/Acomodacao';
 export default {
-  name: "Acomodacao",
-  data() {
-    return {
-      acomodacoes,
-    };
-  },
-  methods: {
-    selecionarAcomodacao(e) {
-      let acomoda = new Acomodacao();
-      acomoda.getAcomodacao(e);
+    name: 'Acomodacao',
+    data() {
+        return {
+            acomodacoes,
+        };
     },
-  },
+    methods: {
+        selecionarAcomodacao(e) {
+            let acomoda = new Acomodacao();
+            acomoda.getAcomodacao(e);
+        },
+    },
 };
 </script>
 
--- src/components/FormFeedback.vue
+++ src/components/FormFeedback.vue
@@ -1,120 +1,154 @@
 <template>
-  <main>
-    <h2 class="title title-feedback">Como foi sua Experiência conosco?</h2>
-    <div class="content-subtitle">
-      <h4 class="subtitle">Conta pra gente!</h4>
-      <img src="@/assets/images/message-alt.svg" alt="Icon Message" />
-    </div>
-    <section class="section">
-      <article class="container">
-        <div class="itens-form">
-          <div class="nomes">
-            <div class="nome1">
-              <label for="Nome">Nome:</label>
-              <input type="text" />
-            </div>
-            <div class="nome2">
-              <label for="Nome">Sobrenome:</label>
-              <input type="text" />
-            </div>
-          </div>
-          <div class="cells">
-            <label for="message">Mensagem:</label>
-            <textarea
-              name="message"
-              class="message"
-              cols="30"
-              rows="10"></textarea>
-          </div>
-          <div class="avaliacao-content">
-            <p>Avalie nossos serviços:</p>
-            <ul class="avaliacao-icons">
-              <li class="star-icon ativo" data-avaliacao="1"></li>
-              <li class="star-icon" data-avaliacao="2"></li>
-              <li class="star-icon" data-avaliacai="3"></li>
-              <li class="star-icon" data-avaliacao="4"></li>
-              <li class="star-icon" data-avaliacao="5"></li>
-            </ul>
-          </div>
-          <input class="btn" type="submit" value="Enviar" />
+    <main>
+        <h2 class="title title-feedback">Como foi sua Experiência conosco?</h2>
+        <div class="content-subtitle">
+            <h4 class="subtitle">Conta pra gente!</h4>
+            <img src="@/assets/images/message-alt.svg" alt="Icon Message" />
         </div>
-      </article>
-    </section>
-  </main>
+        <section class="section">
+            <article class="container">
+                <div class="itens-form">
+                    <div class="nomes">
+                        <div class="nome1">
+                            <label for="nome">Nome:</label>
+                            <input type="text" v-model="nome" name="Nome" />
+                        </div>
+                        <div class="nome2">
+                            <label for="sobrenome">Sobrenome:</label>
+                            <input type="text" v-model="sobrenome" name="sobrenome" />
+                        </div>
+                    </div>
+                    <div class="cells">
+                        <label for="message">Mensagem:</label>
+                        <textarea name="message" v-model="mensagem" class="message" cols="30" rows="10"></textarea>
+                    </div>
+                    <div class="avaliacao-content">
+                        <p>Avalie nossos serviços:</p>
+                        <ul class="avaliacao-icons">
+                            <li class="star-icon ativo" data-avaliacao="1" @click="avaliacao"></li>
+                            <li class="star-icon" data-avaliacao="2" @click="avaliacao"></li>
+                            <li class="star-icon" data-avaliacao="3" @click="avaliacao"></li>
+                            <li class="star-icon" data-avaliacao="4" @click="avaliacao"></li>
+                            <li class="star-icon" data-avaliacao="5" @click="avaliacao"></li>
+                        </ul>
+                    </div>
+                    <input class="btn" type="submit" value="Enviar" @click="submit" />
+                </div>
+            </article>
+
+
+        </section>
+    </main>
 </template>
 
 <script>
+import { AvaliacaoFeedback } from '@/class/AvaliacaoFeedback';
 export default {
-  name: "FormFeedback",
+    name: 'FormFeedback',
+    data() {
+        return {
+            nome: '',
+            sobrenome: '',
+            mensagem: '',
+            notaAvaliacao: '',
+            dadosAvaliacao : []
+        };
+    },
+
+    methods: {
+        avaliacao(e) {
+            this.notaAvaliacao = e.target.getAttribute('data-avaliacao');
+            let avaliacao = new AvaliacaoFeedback();
+            try {
+                avaliacao.avaliacao(e);
+            } catch (erro) {
+                // console.error(erro);
+            }
+        },
+
+        submit() {
+            let avaliacao = new AvaliacaoFeedback();
+            avaliacao.setlocalStorage(this.nome, this.sobrenome, this.notaAvaliacao);
+        },
+        getFeedBack() {
+            let avaliacao = new AvaliacaoFeedback();
+            let dados = avaliacao.getLocalStorage();
+            // this.dadosAvaliacao.push(dados)
+            // console.log(this.dadosAvaliacao);
+        },
+    },
+    mounted() {
+        this.getFeedBack();
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 /* Style de Titles and Form */
 .title-feedback {
-  font-size: 40px;
+    font-size: 40px;
 }
 .content-subtitle {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
 }
 .subtitle {
-  text-align: center;
-  font-size: 20px;
+    text-align: center;
+    font-size: 20px;
 }
 .container {
-  background-color: #eff5f0;
-  width: 700px;
-  border-radius: 12px;
+    background-color: #eff5f0;
+    width: 700px;
+    border-radius: 12px;
 }
 .itens-form {
-  margin: 50px;
+    margin: 50px;
 }
 .btn {
-  margin-top: 10px;
+    margin-top: 10px;
 }
 
 /* Style Star */
 
 .avaliacao-content {
-  display: flex;
-  align-items: center;
-  justify-content: end;
-  flex-direction: column;
-  gap: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    flex-direction: column;
+    gap: 10px;
 }
 
 .avaliacao-icons {
-  margin: 10px 0 20px 0;
-  text-align: right;
-  display: flex;
-  align-items: center;
-  gap: 5px;
+    margin: 10px 0 20px 0;
+    text-align: right;
+    display: flex;
+    align-items: center;
+    gap: 5px;
 }
 
 .star-icon {
-  width: 30px;
-  list-style: none;
-  cursor: pointer;
-  color: #ffe500;
-  font-size: 30px;
+    width: 30px;
+    list-style: none;
+    cursor: pointer;
+    color: #ffe500;
+    font-size: 30px;
 }
 
 .star-icon::before {
-  content: "\2605";
+    content: '\2605';
 }
 
 .star-icon.ativo ~ .star-icon::before {
-  content: "\2606";
+    content: '\2606';
 }
 
 .avaliacao-icons:hover .star-icon::before {
-  content: "\2605";
+    content: '\2605';
 }
 
 .star-icon:hover ~ .star-icon::before {
-  content: "\2606";
+    content: '\2606';
 }
 </style>
