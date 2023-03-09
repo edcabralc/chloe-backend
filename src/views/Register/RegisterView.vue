4ba4549e958db05178a4e70ea3d23fd233c4be4f
rodhis <rodrigonoliva@gmail.com>
2023-03-09 18:38:59 -0300

    primeiro esboço do registro

--- src/views/Register/RegisterView.vue
+++ src/views/Register/RegisterView.vue
@@ -1,47 +1,65 @@
 <template>
-  <main>
-    <hero-banner title="Registro de Novo Usuário" class="register-main" />
-    <main-title>Bem-vindo ao melhor hotel da cidade</main-title>
-    <section class="section">
-      <div class="container">
-        <article class="register">
-          <div class="itens-form">
-            <div class="cells">
-              <label for="Nome">Nome Completo: </label>
-              <input type="text" />
+    <MainHeader />
+    <main>
+        <div class="register-main cover-background">
+            <h1 class="hero-title">Registro de Novo Usuário</h1>
+        </div>
+
+        <h2 class="title">Bem-vindo ao melhor hotel da cidade</h2>
+
+        <section class="section">
+            <div class="container">
+                <article class="register">
+                    <div class="itens-form">
+                        <div class="cells">
+                            <label for="Nome">Nome Completo: </label>
+                            <input type="text" v-bind="nome" />
+                        </div>
+                        <div class="cells">
+                            <label for="email">E-Mail: </label>
+                            <input type="email" v-bind="email" />
+                        </div>
+                        <div class="cells">
+                            <label for="Telefone">Telefone: </label>
+                            <input type="number" v-bind="tel" />
+                        </div>
+                        <div class="cells">
+                            <label for="nascimento">Data de Nascimento </label>
+                            <input type="date" v-bind="date" />
+                        </div>
+                        <div class="cells">
+                            <label for="senha">Senha </label>
+                            <input type="password" v-bind="pass" />
+                        </div>
+                        <input class="btn" type="submit" value="Enviar" @click="register()" />
+                    </div>
+                </article>
             </div>
-            <div class="cells">
-              <label for="email">E-Mail: </label>
-              <input type="email" />
-            </div>
-            <div class="cells">
-              <label for="Telefone">Telefone: </label>
-              <input type="number" />
-            </div>
-            <div class="cells">
-              <label for="nascimento">Data de Nascimento </label>
-              <input type="date" />
-            </div>
-            <div class="cells">
-              <label for="senha">Senha </label>
-              <input type="password" />
-            </div>
-            <input class="btn" type="submit" value="Enviar" />
-          </div>
-        </article>
-      </div>
-    </section>
-  </main>
+        </section>
+    </main>
+    <MainFooter />
 </template>
 
 <script>
-import HeroBanner from "@/components/commons/HeroBanner";
-import MainTitle from "@/components/commons/MainTitle";
+import MainFooter from '../../components/MainFooter.vue'
+import MainHeader from '../../components/MainHeader.vue'
+import newRegister from '../../helpers/newRegister.js'
 
 export default {
-  name: "RegisterView",
-  components: { HeroBanner, MainTitle },
-};
+    name: 'RegisterView',
+    components: { MainHeader, MainFooter, newRegister },
+    data() {
+        newUser = {
+            nome: nome,
+            email: email,
+            dataNasc: date,
+            pwd: pass,
+        }
+    },
+    props: {
+      newUser: {}
+    }
+}
 </script>
 
-<style src="./style.scss" lang="scss" scoped />
+<style src="./style.scss" lang="scss" />
