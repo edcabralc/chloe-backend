0b78e10cdcec9ba540ad86a9aa1dc9f61f7f96ff
edcabralc <cabralnet@gmail.com>
2023-03-12 18:54:55 -0300

    add: rotas adicionar, editar e remover usuario

--- admin/api/src/routes/routes.ts
+++ admin/api/src/routes/routes.ts
@@ -8,9 +8,10 @@ router.get('/ping', (req, res) => {
     res.json({ pong: true })
 })
 
-router.get('/usuarios', UserController.getAll)
+router.post('/usuarios', UserController.getAll)
+router.post('/usuario/registrar', UserController.createUser)
 router.post('/usuario/:id', UserController.getById)
-router.put('/usuario/:id', UserController.editById)
+router.put('/usuario/editar/:id', UserController.editById)
 router.delete('/usuario/remover/:id', UserController.deleteById)
 
 router.get('/acomodacoes', RoomController.getAll)
