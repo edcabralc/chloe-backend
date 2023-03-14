e267ce0306694d7ee08510140c28e79eb3b11799
rodhis <rodrigonoliva@gmail.com>
2023-03-14 17:57:30 -0300

    update dir

--- src/views/Register/RegisterView.vue
+++ src/views/Register/RegisterView.vue
@@ -13,23 +13,23 @@
                     <div class="itens-form">
                         <div class="cells">
                             <label for="Nome">Nome Completo: </label>
-                            <input type="text" v-model="nome" />
+                            <input type="text" v-bind:name="newUser.nome" />
                         </div>
                         <div class="cells">
                             <label for="email">E-Mail: </label>
-                            <input type="email" v-model="email" />
+                            <input type="email" v-bind:name="newUser.email" />
                         </div>
                         <div class="cells">
                             <label for="Telefone">Telefone: </label>
-                            <input type="number" v-model="tel" />
+                            <input type="number" v-bind:name="tel"/>
                         </div>
                         <div class="cells">
                             <label for="nascimento">Data de Nascimento </label>
-                            <input type="date" v-model="date" />
+                            <input type="date" v-bind:name="date" />
                         </div>
                         <div class="cells">
                             <label for="senha">Senha </label>
-                            <input type="password" v-model="pass" />
+                            <input type="password" v-bind:name="pass" />
                         </div>
                         <input class="btn" type="submit" value="Enviar" @click="register()" />
                     </div>
