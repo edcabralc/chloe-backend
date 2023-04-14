26a78ae911d8ad9ad19e38f6a17b4d1a07f75c59
Rodhis <rodrigonoliva@gmail.com>
2023-04-14 16:40:32 -0300

    sql inj protection

--- src/views/Register/RegisterView.vue
+++ src/views/Register/RegisterView.vue
@@ -12,15 +12,15 @@
                         <div class="itens-form">
                             <div class="cells">
                                 <label for="nome_usuario">Nome Completo:</label>
-                                <input type="text" name="nome_usuario" v-model="nome_usuario" />
+                                <input type="text" name="nome_usuario" v-model="user.nome_usuario" />
                             </div>
                             <div class="cells">
                                 <label for="email_usuario">Email:</label>
-                                <input type="email" name="email_usuario" v-model="email_usuario" />
+                                <input type="email" name="email_usuario" v-model="user.email_usuario" />
                             </div>
                             <div class="cells">
                                 <label for="password">Senha:</label>
-                                <input type="password" name="password" v-model="password" />
+                                <input type="password" name="password" v-model="user.password" />
                             </div>
                             <input class="btn" type="submit" value="Enviar" @click.prevent="register()" />
                         </div>
@@ -38,53 +38,57 @@ import { mapState, mapActions } from 'vuex'
 
 export default {
     name: 'RegisterView',
-    name: 'RegisterView',
-    computed: {
-        // ...mapState(['user']),
-        nome_usuario: {
-            get() {
-                return this.$store.state.user.nome_usuario
-                return this.$store.state.user.nome_usuario
+    data() {
+        return {
+            user: {
+                nome_usuario: '',
+                email_usuario: '',
+                password: '',
             },
-
-            set(userTrim) {
-                this.$store.commit('updateUser', { nome_usuario: userTrim })
-            },
-        },
-        email_usuario: {
-            get() {
-                return this.$store.state.user.email_usuario
-                return this.$store.state.user.email_usuario
-            },
-
-            set(mailTrim) {
-                this.$store.commit('updateUser', { email_usuario: mailTrim })
-            },
-        },
-        password: {
-            get() {
-                return this.$store.state.user.password
-                return this.$store.state.user.password
-            },
-            set(value) {
-                this.$store.commit('updateUser', { password: value })
-                this.$store.commit('updateUser', { password: value })
-            },
-        },
+        }
     },
+    // computed: {
+    //     // ...mapState(['user']),
+    //     nome_usuario: {
+    //         get() {
+    //             return this.$store.state.user.nome_usuario
+    //         },
+
+    //         set(userTrim) {
+    //             this.$store.commit('updateUser', { nome_usuario: userTrim })
+    //         },
+    //     },
+    //     email_usuario: {
+    //         get() {
+    //             return this.$store.state.user.email_usuario
+    //         },
+
+    //         set(mailTrim) {
+    //             this.$store.commit('updateUser', { email_usuario: mailTrim })
+    //         },
+    //     },
+    //     password: {
+    //         get() {
+    //             return this.$store.state.user.password
+    //         },
+    //         set(value) {
+    //             this.$store.commit('updateUser', { password: value })
+    //         },
+    //     },
+    // },
     methods: {
-        // ...mapActions(['register']),
-        removeQuotesSpaces(str) {
-            return str.replaceAll("'", '').replaceAll('"', '').trim()
+        //usando método regex de prevenção de sql injection direto ao digitar
+        injTreatment(str) {
+            return str.replace(/[''"]/g, '')
         },
-
         register() {
-            const userTrim = this.removeQuotesSpaces(this.nome_usuario)
-            const mailTrim = this.removeQuotesSpaces(this.email_usuario)
-            this.$store.dispatch('createUser', this.$store.state.user)
+            this.$store.dispatch('createUser', this.user)
         },
     },
-    components: { HeroBanner },
+    updated() {
+        this.user.nome_usuario = this.injTreatment(this.user.nome_usuario)
+        this.user.email_usuario = this.injTreatment(this.user.email_usuario)
+    },
     components: { HeroBanner },
 }
 </script>
